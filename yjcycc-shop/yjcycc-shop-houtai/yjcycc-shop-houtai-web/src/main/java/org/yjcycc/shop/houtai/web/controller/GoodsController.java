c28627a2296a8f9f6b3709a147e1fe3b9a243341
yangjun <yangjun@ihavecar.com>
2017-04-11 18:13:56 +0800

    优化

--- yjcycc-shop/yjcycc-shop-houtai/yjcycc-shop-houtai-web/src/main/java/org/yjcycc/shop/houtai/web/controller/GoodsController.java
+++ yjcycc-shop/yjcycc-shop-houtai/yjcycc-shop-houtai-web/src/main/java/org/yjcycc/shop/houtai/web/controller/GoodsController.java
@@ -6,6 +6,11 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.yjcycc.shop.goods.client.GoodsClient;
+import org.yjcycc.shop.goods.entity.Goods;
+import org.yjcycc.shop.goods.service.GoodsService;
+
+import com.github.pagehelper.Page;
 
 @RestController
 @RequestMapping(value = "/goods")
@@ -17,7 +22,17 @@ public class GoodsController {
 	public Object list(HttpServletRequest request) {
 		logger.info("start.");
 		
-		
+		GoodsService goodsService = (GoodsService) GoodsClient.getRemoteService("GoodsService");
+        
+        try {
+	        Page<Goods> pager = goodsService.findPager("aa");
+	        for (Goods goods : pager) {
+	        	System.out.println(goods.getName());
+	        }
+	        System.out.println(pager.getEndRow());
+        } catch (Exception e) {
+        	e.printStackTrace();
+        }
 		
 		return null;
 	}
